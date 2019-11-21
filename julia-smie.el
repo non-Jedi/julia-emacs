9d3b6818aa44d19d6f1ffa39258d0c5edfd41749
Adam Beckmeyer <adam_gpg@thebeckmeyers.xyz>
2019-11-21 16:04:52 -0500

    WIP: add skip-ws function and start work on next-token

--- julia-smie.el
+++ julia-smie.el
@@ -5,6 +5,7 @@
   (concatenate 'list ops (map 'list (lambda (op) (concat "." op)) ops)))
 
 (eval-when-compile
+  ;; TODO: use symbols instead of strings for operators?
   (defconst julia-mode-prec-assignment
     (concatenate 'list
                  '(":=" "$=")
@@ -94,10 +95,42 @@
     julia-mode-prec-rational julia-mode-prec-bitshift julia-mode-prec-power
     julia-mode-prec-decl julia-mode-prec-dot))
 
+(defconst julia-mode--whitespace-chars
+  (string 9 11 12 13 32 133 160 5760 6158 8192 8193 8194 8195 8196 8197 8198
+          8199 8200 8201 8202 8232 8233 8239 8287 12288))
+(defconst julia-mode--whitespace-chars+ (concat julia-mode--whitespace-chars "\n"))
+
+(defconst julia-smie-syntax-table
+  (let ((table (make-syntax-table)))
+    (mapc (lambda (ch) (modify-syntax-entry ch "-" table))
+          julia-mode--whitespace-chars)
+    table))
+
+;; Implements function skipws/skip-ws assuming julia-smie-syntax-table
+;; is current syntax table:
+;; https://github.com/JuliaLang/JuliaParser.jl/blob/a3a152547a84748e266cbce330f64cd1820e0bde/src/lexer.jl#L373
+(defun julia-mode--skip-ws (newlines)
+  ;; TODO: wrap in (with-syntax-table julia-smie-syntax-table ...) ?
+  (not (zerop (if newlines (skip-chars-forward julia-mode--whitespace-chars+)
+                (skip-syntax-forward "-")))))
+
+(defun julia-mode--next-token (s)
+  "Return next token and move point to end of token given lexing state S.
+Instead of operating on a string like the original does, this function
+directly manipulates the emacs buffer."
+  (aset s 2 (julia-mode--skip-ws t))
+  ;; This follows JuliaParser.jl's next_token implementation for EOF checks
+  (if (eobp)
+      'EOF
+    (let ((c (char-after)))))) ; TODO...
+
 ;;; token stream
 
-(defun julia-mode--make-token-stream (s) (vector nil s t nil nil))
-(defmacro julia-mode--ts:port (s) `(aref ,s 1))
+;; element 1 of vector is the string/iobuffer in original parser. For
+;; consistency, it will always remain nil in this implementation (see
+;; julia-mode--next-token docstring).
+(defun julia-mode--make-token-stream () (vector nil nil t nil nil))
+;;(defmacro julia-mode--ts:port (s) `(aref ,s 1))
 (defmacro julia-mode--ts:last-tok (s) `(aref ,s 0))
 (defmacro julia-mode--ts:set-tok! (s tok) `(aset ,s 0 ,tok))
 (defmacro julia-mode--ts:pbtok (s) `(aref ,s 3))
